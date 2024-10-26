1c85330549e8e58ebd146a0a6c5a531a2880ed0b
sapphir0v <6.sapphirov.6@bk.ru>
2024-10-26 15:42:09 +0300

    first shell

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,44 @@
 public class Employee {
+    private String fullName;
+    private int departament;
+    private int salary;
+    private static int ID;
+
+    public Employee(String fullName, int departament, int salary) {
+        this.fullName = fullName;
+        this.departament = departament;
+        this.salary = salary;
+        Employee.ID++;
+        this.ID = ID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartament() {
+        return departament;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public static int getID() {
+        return ID;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setDepartament(int departament) {
+        this.departament = departament;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
 
 }
